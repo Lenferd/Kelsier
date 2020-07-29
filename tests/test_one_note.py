b813bb9700638698d2819bf66724de4aff0759bd
lenferd <Deysonsh@gmail.com>
2020-07-30 00:10:36 +0300

    [Tests] Move all tests to same folder

---
+++ tests/test_one_note.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestOneNote(TestCase):
+  def test__get_all_notebooks(self):
+    self.fail()
+
+  def test__search_for_list_url(self):
+    self.fail()
+
+  def test_add_task(self):
+    self.fail()
